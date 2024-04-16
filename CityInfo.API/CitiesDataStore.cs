6de058a0bc174f7a91b3903d67648a4731b01bef
Alireza Ghiasy <sina121gh@gmail.com>
2024-04-16 22:10:13 +0330

    initial commit

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,38 @@
+﻿using CityInfo.API.Models.DTOs;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Tehran",
+                    Description = "The capital of Iran",
+                },
+
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Kashan",
+                    Description = "My City",
+                },
+
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Shiraz",
+                    Description = "a beautiful city",
+                },
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using CityInfo.API.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<CityDto> GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .SingleOrDefault(c => c.Id == id);
+
+            if (city == null)
+                return NotFound();
+
+            return Ok(city);
+        }
+    }
+}
