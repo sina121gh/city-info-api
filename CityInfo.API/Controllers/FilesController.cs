80695249afd9bc60b79f1b1352d3c5d34fc8dd7a
Alireza Ghiasy-L <sina121gh@gmail.com>
2024-04-18 10:07:53 +0330

    added files controller and download file action

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        #region Dependency Injection
+
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider;
+        }
+
+        #endregion
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            string filePath = "WebApiBanner.jpg";
+
+            if (!System.IO.File.Exists(filePath))
+                return NotFound();
+
+            byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(filePath, out string contentType))
+                contentType = "application/octet-stream";
+
+            return File(bytes, contentType, Path.GetFileName(filePath));
+        }
+    }
+}
--- CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f44ed260ae193d26a47bf2c059dbfed1f005d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c0fd86ebbb48088eecb0c5a1891a1e53fddf8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
